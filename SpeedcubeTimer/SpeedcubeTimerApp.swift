7b40456b9150b2bd4ab1a06b2d241aa94eee1d44
Kacper Wybrański <kacper.wybranski@bankmillennium.pl>
2022-03-22 14:46:49 +0100

    Initial Commit

---
+++ SpeedcubeTimer/SpeedcubeTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpeedcubeTimerApp.swift
+//  SpeedcubeTimer
+//
+//  Created by x210981 on 22/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct SpeedcubeTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
