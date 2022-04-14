a27b78f3cc2625275d8ea931cf710e7269be97cf
KacperWybranski <kacperwybranski@gmail.com>
2022-04-14 08:38:52 +0200

    Remove comments

--- SpeedcubeTimer/Application/Model/CubingSession.swift
+++ SpeedcubeTimer/Application/Model/CubingSession.swift
@@ -8,33 +8,6 @@
 import Foundation
 import SwiftUI
 
-//class CubingSession: ObservableObject, Equatable {
-//    @Published var results: [Result]
-//    @Published var cube: Cube
-//    @Published var sessionindex: Int
-//
-//    var bestResult: Result? {
-//        results.best
-//    }
-//
-//    init(results: [Result], cube: Cube, session: Int) {
-//        self.results = results
-//        self.cube = cube
-//        self.sessionindex = session
-//
-//    }
-//
-//    static var initialSession: CubingSession {
-//        return CubingSession(results: [], cube: .three, session: 1)
-//    }
-//
-//    static func == (lhs: CubingSession, rhs: CubingSession) -> Bool {
-//        return (lhs.results == rhs.results &&
-//                lhs.cube == rhs.cube &&
-//                lhs.sessionindex == rhs.sessionindex)
-//    }
-//}
-
 struct CubingSession: Equatable {
     var results: [Result]
     var cube: Cube
