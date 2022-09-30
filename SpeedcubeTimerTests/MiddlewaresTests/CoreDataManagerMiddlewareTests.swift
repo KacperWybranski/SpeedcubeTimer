8146f6318c5b1383a9aa752e1acdee1298ddc936
KacperWybranski <kacperwybranski@gmail.com>
2022-09-30 16:02:38 +0200

    Middleware core data tests

--- SpeedcubeTimerTests/MiddlewaresTests/CoreDataManagerMiddlewareTests.swift
+++ SpeedcubeTimerTests/MiddlewaresTests/CoreDataManagerMiddlewareTests.swift
@@ -69,6 +69,258 @@ final class CoreDataManagerMiddlewareTests: XCTestCase {
         wait(for: [expectation1, expectation2], timeout: 5.0)
     }
     
+    func testSaveResult() {
+        
+        // Input
+        
+        let expectation1 = XCTestExpectation(description: "on receive value")
+        let expectation2 = XCTestExpectation(description: "on action recognized")
+        let expectation3 = XCTestExpectation(description: "on save")
+        
+        let initialSession = CubingSession.initialSession
+        let beforeState = AppState(allSessions: [initialSession],
+                                   currentSession: initialSession,
+                                   screens: [
+                                    .timerScreen(TimerViewState(session: initialSession)),
+                                    .resultsScreen(ResultsViewState(currentSession: initialSession)),
+                                    .settingsScreen(SettingsViewState (allSessions: [initialSession]))
+                                   ])
+        let newResult = Result(time: 8.0, scramble: "scramble", date: Date())
+        
+        // On action
+        
+        dataController?.onSave = { result, session in
+            expectation3.fulfill()
+            XCTAssertEqual(session, initialSession)
+            XCTAssertEqual(result, newResult)
+            
+        }
+        
+        let onReceiveValue: ((Action) -> Void) = { action in
+            expectation1.fulfill()
+            if case AppStateAction.loadSessions = action {
+                expectation2.fulfill()
+            }
+        }
+        
+        // Perform
+        
+        let key = UUID()
+        
+        coreDataManager?(
+            beforeState,
+            TimerViewStateAction.saveResult(newResult)
+        )
+        .receive(on: RunLoop.main)
+        .sink(receiveCompletion: { [weak self] _ in
+            self?.subscriptions.removeValue(forKey: key)
+        }, receiveValue: { action in
+            onReceiveValue(action)
+        })
+        .store(in: &subscriptions, key: key)
+        
+        wait(for: [expectation1, expectation3], timeout: 5.0)
+    }
+    
+    func testRemoveResults() {
+        // Input
+        
+        let expectation1 = XCTestExpectation(description: "on receive value")
+        let expectation2 = XCTestExpectation(description: "on action recognized")
+        let expectation3 = XCTestExpectation(description: "on remove")
+        
+        let initialSession = TestConfiguration.sessions.first!
+        let beforeState = AppState(allSessions: [initialSession],
+                                   currentSession: initialSession,
+                                   screens: [
+                                    .timerScreen(TimerViewState(session: initialSession)),
+                                    .resultsScreen(ResultsViewState(currentSession: initialSession)),
+                                    .settingsScreen(SettingsViewState (allSessions: [initialSession]))
+                                   ])
+        
+        // On action
+        
+        dataController?.onRemove = { result, session in
+            expectation3.fulfill()
+            XCTAssertEqual(session, initialSession)
+            XCTAssertEqual(result, initialSession.results.first)
+            
+        }
+        
+        let onReceiveValue: ((Action) -> Void) = { action in
+            expectation1.fulfill()
+            if case AppStateAction.loadSessions = action {
+                expectation2.fulfill()
+            }
+        }
+        
+        // Perform
+        
+        let key = UUID()
+        
+        coreDataManager?(
+            beforeState,
+            ResultsViewStateAction.removeResultsAt(IndexSet(integer: 0))
+        )
+        .receive(on: RunLoop.main)
+        .sink(receiveCompletion: { [weak self] _ in
+            self?.subscriptions.removeValue(forKey: key)
+        }, receiveValue: { action in
+            onReceiveValue(action)
+        })
+        .store(in: &subscriptions, key: key)
+        
+        wait(for: [expectation1, expectation3], timeout: 5.0)
+    }
+    
+    func testNameChanged() {
+        // Input
+        
+        let expectation1 = XCTestExpectation(description: "on receive value")
+        let expectation2 = XCTestExpectation(description: "on action recognized")
+        let expectation3 = XCTestExpectation(description: "on name change")
+        
+        let initialSession = TestConfiguration.sessions.first!
+        let beforeState = AppState(allSessions: [initialSession],
+                                   currentSession: initialSession,
+                                   screens: [
+                                    .timerScreen(TimerViewState(session: initialSession)),
+                                    .resultsScreen(ResultsViewState(currentSession: initialSession)),
+                                    .settingsScreen(SettingsViewState (allSessions: [initialSession]))
+                                   ])
+        
+        // On action
+        
+        dataController?.onChangeName = { session, name in
+            expectation3.fulfill()
+            XCTAssertEqual(session, initialSession)
+            XCTAssertEqual(name, TestConfiguration.sessionName)
+            
+        }
+        
+        let onReceiveValue: ((Action) -> Void) = { action in
+            expectation1.fulfill()
+            if case AppStateAction.loadSessions = action {
+                expectation2.fulfill()
+            }
+        }
+        
+        // Perform
+        
+        let key = UUID()
+        
+        coreDataManager?(
+            beforeState,
+            SettingsViewStateAction.currentSessionNameChanged(TestConfiguration.sessionName)
+        )
+        .receive(on: RunLoop.main)
+        .sink(receiveCompletion: { [weak self] _ in
+            self?.subscriptions.removeValue(forKey: key)
+        }, receiveValue: { action in
+            onReceiveValue(action)
+        })
+        .store(in: &subscriptions, key: key)
+        
+        wait(for: [expectation1, expectation3], timeout: 5.0)
+    }
+    
+    func testErase() {
+        // Input
+        
+        let expectation1 = XCTestExpectation(description: "on receive value")
+        let expectation2 = XCTestExpectation(description: "on action recognized")
+        let expectation3 = XCTestExpectation(description: "on erase")
+        
+        let initialSession = TestConfiguration.sessions.first!
+        let beforeState = AppState(allSessions: [initialSession],
+                                   currentSession: initialSession,
+                                   screens: [
+                                    .timerScreen(TimerViewState(session: initialSession)),
+                                    .resultsScreen(ResultsViewState(currentSession: initialSession)),
+                                    .settingsScreen(SettingsViewState (allSessions: [initialSession]))
+                                   ])
+        
+        // On action
+        
+        dataController?.onErase = { session in
+            expectation3.fulfill()
+            XCTAssertEqual(session, initialSession)
+            
+        }
+        
+        let onReceiveValue: ((Action) -> Void) = { action in
+            expectation1.fulfill()
+            if case AppStateAction.loadSessions = action {
+                expectation2.fulfill()
+            }
+        }
+        
+        // Perform
+        
+        let key = UUID()
+        
+        coreDataManager?(
+            beforeState,
+            SettingsViewStateAction.eraseSession(initialSession)
+        )
+        .receive(on: RunLoop.main)
+        .sink(receiveCompletion: { [weak self] _ in
+            self?.subscriptions.removeValue(forKey: key)
+        }, receiveValue: { action in
+            onReceiveValue(action)
+        })
+        .store(in: &subscriptions, key: key)
+        
+        wait(for: [expectation1, expectation3], timeout: 5.0)
+    }
+    
+    func testResetApp() {
+        // Input
+        
+        let expectation1 = XCTestExpectation(description: "on receive value")
+        let expectation2 = XCTestExpectation(description: "on action recognized")
+        let expectation3 = XCTestExpectation(description: "on reset")
+        
+        let initialSession = TestConfiguration.sessions.first!
+        let beforeState = AppState(allSessions: [initialSession],
+                                   currentSession: initialSession,
+                                   screens: [
+                                    .timerScreen(TimerViewState(session: initialSession)),
+                                    .resultsScreen(ResultsViewState(currentSession: initialSession)),
+                                    .settingsScreen(SettingsViewState (allSessions: [initialSession]))
+                                   ])
+        
+        // On action
+        
+        dataController?.onReset = {
+            expectation3.fulfill()
+        }
+        
+        let onReceiveValue: ((Action) -> Void) = { action in
+            expectation1.fulfill()
+            if case AppStateAction.loadSessions = action {
+                expectation2.fulfill()
+            }
+        }
+        
+        // Perform
+        
+        let key = UUID()
+        
+        coreDataManager?(
+            beforeState,
+            SettingsViewStateAction.resetApp
+        )
+        .receive(on: RunLoop.main)
+        .sink(receiveCompletion: { [weak self] _ in
+            self?.subscriptions.removeValue(forKey: key)
+        }, receiveValue: { action in
+            onReceiveValue(action)
+        })
+        .store(in: &subscriptions, key: key)
+        
+        wait(for: [expectation1, expectation3], timeout: 5.0)
+    }
 }
 
 final class TestDataController: DataControllerProtocol {
@@ -117,7 +369,8 @@ private enum TestConfiguration {
                        scramble: "abc",
                        date: Date())],
             cube: .four,
-            index: 3
+            index: 3,
+            name: "Hello"
         ),
         .init(
             results: [
@@ -131,4 +384,5 @@ private enum TestConfiguration {
             index: 3
         )
     ]
+    static let sessionName = "Test name"
 }
