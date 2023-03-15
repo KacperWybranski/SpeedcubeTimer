f3b1e7a0e34bfda7056aedf874c9e7d00029e62a
KacperWybranski <kacperwybranski@gmail.com>
2023-03-15 10:01:43 +0100

    more tests

--- SpeedcubeTimerTests/ReducersTests/TimerReducerTests.swift
+++ SpeedcubeTimerTests/ReducersTests/TimerReducerTests.swift
@@ -42,6 +42,126 @@ final class TimerReducerTests: XCTestCase {
             state.cube = Configuration.session.cube
         }
     }
+    
+    @MainActor
+    func testSaveResultWithoutRecord() async {
+        let store = TestStore(
+            initialState: TimerFeature.State(),
+            reducer: TimerFeature(
+                mainQueue: .immediate,
+                overlayCheckPriority: .userInitiated,
+                sessionsManager: sessionManager,
+                userSettings: userSettings
+            )
+        )
+        
+        sessionManager.checkForPbResult = Configuration.checkForPbNoneResult
+        
+        _ = await store.send(.saveResult(Configuration.result))
+        
+        await store.receive(.newRecordSet(Configuration.checkForPbNoneResult))
+    }
+    
+    @MainActor
+    func testSaveResultWithRecord() async {
+        let store = TestStore(
+            initialState: TimerFeature.State(),
+            reducer: TimerFeature(
+                mainQueue: .immediate,
+                overlayCheckPriority: .userInitiated,
+                sessionsManager: sessionManager,
+                userSettings: userSettings
+            )
+        )
+        
+        sessionManager.checkForPbResult = Configuration.checkForPbAvg5Result
+        
+        _ = await store.send(.saveResult(Configuration.result))
+        
+        await store.receive(.newRecordSet(Configuration.checkForPbAvg5Result))
+    }
+    
+    @MainActor
+    func testSolvingProcess() async {
+        let store = TestStore(
+            initialState: TimerFeature.State(
+                cubingState: .idle,
+                time: 0.0,
+                cube: .three,
+                scramble: .empty,
+                alert: nil
+            ),
+            reducer: TimerFeature(
+                mainQueue: .immediate,
+                overlayCheckPriority: .userInitiated,
+                sessionsManager: sessionManager,
+                userSettings: userSettings
+            )
+        )
+        store.exhaustivity = .off
+        
+        _ = await store.send(.touchBegan) {
+            $0.cubingState = .ready
+        }
+        
+        _ = await store.send(.touchEnded) {
+            $0.cubingState = .ongoing
+        }
+        
+        _ = await store.send(.touchBegan) {
+            $0.cubingState = .ended
+        }
+        
+        _ = await store.send(.touchEnded) {
+            $0.cubingState = .idle
+        }
+    }
+    
+    @MainActor
+    func testSolvingProcessWithPreinspection() async {
+        let store = TestStore(
+            initialState: TimerFeature.State(
+                cubingState: .idle,
+                time: 0.0,
+                cube: .three,
+                scramble: .empty,
+                alert: nil
+            ),
+            reducer: TimerFeature(
+                mainQueue: .immediate,
+                overlayCheckPriority: .userInitiated,
+                sessionsManager: sessionManager,
+                userSettings: userSettings
+            )
+        )
+        store.exhaustivity = .off
+        
+        userSettings.setIsPreinspectionOn(true)
+        
+        _ = await store.send(.touchBegan) {
+            $0.cubingState = .ready
+        }
+        
+        _ = await store.send(.touchEnded) {
+            $0.cubingState = .preinspectionOngoing
+        }
+        
+        _ = await store.send(.touchBegan) {
+            $0.cubingState = .preinspectionReady
+        }
+        
+        _ = await store.send(.touchEnded) {
+            $0.cubingState = .ongoing
+        }
+        
+        _ = await store.send(.touchBegan) {
+            $0.cubingState = .ended
+        }
+        
+        _ = await store.send(.touchEnded) {
+            $0.cubingState = .idle
+        }
+    }
 }
 
 private enum Configuration {
@@ -52,4 +172,13 @@ private enum Configuration {
         name: nil,
         id: .init()
     )
+    
+    static var result: Result = .init(
+        time: 2.0,
+        scramble: "scramble",
+        date: Date()
+    )
+    
+    static var checkForPbAvg5Result: OverlayManager.RecordType = .avg5
+    static var checkForPbNoneResult: OverlayManager.RecordType = .none
 }
